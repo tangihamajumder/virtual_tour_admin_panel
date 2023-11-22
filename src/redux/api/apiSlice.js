fde9773c805de86c681742b55f62001e26354be7
tangihamajumder <tangihamajumder90@gmail.com>
2023-11-22 09:50:55 -0500

    link change

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'https://virtual-tour-backend.vercel.app/api/v1/',
-        baseUrl: 'http://localhost:8080/api/v1/',
+        baseUrl: 'https://virtual-tour-backend.vercel.app/api/v1/',
+        // baseUrl: 'http://localhost:8080/api/v1/',
     }),
     tagTypes: ['courses','instructors','testimonials'],
     endpoints: () => ({}),
